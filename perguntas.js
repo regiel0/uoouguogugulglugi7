23e96cf7ac22412ef72e1752434716d13d47f231
regiel0 <163352495+regiel0@users.noreply.github.com>
2024-10-08 10:56:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScript',
+    'O JavaScript é uma inguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França',
+    'A capital da França é Paris',
+)
+criaCartao(
+    'Programação',
+        'O que é uma função?',
+        'Uma é bloco de código que executa alguma tarefa'
+)
+criaCartao(
+    'Lingua Ingles',
+    'Como se diz oi em inglês?',
+    'Oi em inglês se diz Hi (rai)'
+)
